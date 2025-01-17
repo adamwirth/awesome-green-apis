3805fe84c3bb6e96ca5b1cd4784d682e4fbb6c68
Adam Wirth <adamowirth@gmail.com>
2025-01-17 12:59:19 -0500

    removing more unused amplify things

--- amplify/data/models/firststreet_air_factor.ts
+++
@@ -1,9 +0,0 @@
-import { a } from "@aws-amplify/backend";
-
-export const AirFactorSchema = a
-  .model({
-    fsid: a.integer(),
-    airFactor: a.integer(),
-    factorScale: a.enum(['Minimal', 'Minor', 'Moderate', 'Major', 'Severe', 'Extreme']),
-  })
-  .authorization(allow => allow.guest().to(['read']));
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
 "use client";
 
 import { useState } from "react";
-import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
 
 import Charts from "./components/charts";
 import Header, { VisualizationType } from "./components/header";
 import { DefaultDarkMode } from "./dark_mode";
-
-Amplify.configure(outputs);
 
 
 export default function App() {
